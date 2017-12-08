638ab976da2cc873271c48c4b6d6dc3b2d6e90d8
Julien Deniau <julien.deniau@gmail.com>
2017-12-08 16:19:59 +0100

    run prettier on config file

--- src/config.js
+++ src/config.js
@@ -8,8 +8,8 @@ export default {
       avatar: './demo/alice.jpg',
       gif: [
         './demo/alice.gif',
-        'https://media3.giphy.com/media/ZXKZWB13D6gFO/giphy.gif'
-      ]
+        'https://media3.giphy.com/media/ZXKZWB13D6gFO/giphy.gif',
+      ],
     },
     {
       name: 'Bob',
@@ -17,32 +17,32 @@ export default {
       gif: [
         './demo/bob.gif',
         'https://media.giphy.com/media/u6B4sb945KDlK/giphy.gif',
-        'https://media.giphy.com/media/l396BGuJksuiWBd8A/giphy.gif'
-      ]
+        'https://media.giphy.com/media/l396BGuJksuiWBd8A/giphy.gif',
+      ],
     },
     {
       name: 'Charlie',
       avatar: './demo/charlie.jpg',
       gif: [
         './demo/charlie.gif',
-        'https://media.giphy.com/media/13sozYO4hmSMUw/giphy.gif'
-      ]
+        'https://media.giphy.com/media/13sozYO4hmSMUw/giphy.gif',
+      ],
     },
     {
       name: 'Daniel',
       avatar: './demo/daniel.jpg',
       gif: [
         './demo/daniel.gif',
-        'https://media.giphy.com/media/HBLi8VigKrnDG/giphy.gif'
-      ]
+        'https://media.giphy.com/media/HBLi8VigKrnDG/giphy.gif',
+      ],
     },
     {
       name: 'Eve',
       avatar: './demo/eve.jpg',
       gif: [
         './demo/eve.gif',
-        'https://media.giphy.com/media/Fdsxpt2wIlKtG/giphy.gif'
-      ]
-    }
-  ]
+        'https://media.giphy.com/media/Fdsxpt2wIlKtG/giphy.gif',
+      ],
+    },
+  ],
 };
